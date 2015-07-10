2efa0d4617458717676aba0ade3f0c3498b1ebcb
Artur Kowalski <artkow91@gmail.com>
2015-07-10 21:31:00 +0200

    dodanie header'a oraz dodanie funkcji wyboru wykonywanego dzialania

--- Kalkulator_PD2/src/Kalkulator_PD2.cpp
+++ Kalkulator_PD2/src/Kalkulator_PD2.cpp
@@ -7,9 +7,35 @@
 //============================================================================
 
 #include <iostream>
+#include <string>
+#include "header.h"
+
 using namespace std;
 
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	float liczba1 = 0;
+	float liczba2 = 0;
+	string dzialanie;
+	cout << "Proszę wybrac działanie:" << endl;
+	cout << "dodawanie     odejmowanie     mnożenie     dzielenie" << endl;
+
+	cin >> dzialanie;
+
+	cout << "Proszę podac dwie liczby, na których zostanie wykonane wybrane działanie." << endl;
+
+	if(dzialanie == "dodawanie"){
+
+	}else if(dzialanie == "odejmowanie"){
+
+	}else if(dzialanie == "mnożenie"){
+
+	}else if(dzialanie == "dzielenie"){
+
+	}else{
+		cout << "Nie została wybrana żadna opcja (ewentualnie nastąpił błąd w pisowni)." << endl;
+		cout << "Spróbuj jeszcze raz!" << endl;
+	}
 	return 0;
 }
+
---
+++ Kalkulator_PD2/src/header.h
@@ -0,0 +1,15 @@
+/*
+ * header.h
+ *
+ *  Created on: 10 lip 2015
+ *      Author: Artur
+ */
+
+#ifndef HEADER_H_
+#define HEADER_H_
+
+
+
+
+
+#endif /* HEADER_H_ */
